f9a44d3b5ca36d972629245175163ffbeae3ec0f
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2024-08-23 07:50:27 -0600

    Creacion de la interfaz
    Se agragaron los comentarios de cada paso

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+//Desabilitamos el boton de desencriptar al principio
+const btn_decrypt = document.querySelector(".main__input__botones__desencriptar");
+btn_decrypt.disabled = true;
+btn_decrypt.style["background-color"] = "#D8DFE8";
+
+//hacemos la funcion de si se da un click, se borre la informacion
+const input = document.querySelector(".main__input__text");
+input.onclick = function() {
+    input.value = "";
+}
+
+// funcion script del boton encriptar
+const btn_encrypt = document.querySelector(".main__input__botones__encriptar");
+btn_encrypt.onclick = function() {
+    const selector = document.querySelector(".main__output");
+
+    const image = document.querySelector(".main__output__image");
+    image.style.display = 'none';
+    
+    const titulo = document.querySelector(".main__output__titulo");
+    titulo.innerHTML = "";
+
+    const texto = document.querySelector(".main__output__texto");
+    texto.innerHTML = "";
+
+    let string = input.value;
+    texto.style["font-size"] = "2rem";
+
+    /*Hacemos que el texto que se muestre abarque todo el output y que haga un salto de linea*/
+    texto.style["width"] = "100%";
+    texto.style["overflow-wrap"] = "break-word";
+    /*--------------------------------------------------------------------------------------*/
+    /*Usamos la flexbox para acomodar el texto arriba a la izquierda*/
+    selector.style["justify-content"] = "flex-start";
+    selector.style["align-items"] = "flex-start";
+    /*--------------------------------------------------------------------------------------*/
+    texto.innerHTML = string;
+
+    const copiar = document.createElement("button");
+    copiar.innerHTML = "Copiar";
+    copiar.className = "main__output__copiar";
+
+    selector.appendChild(copiar);
+}
